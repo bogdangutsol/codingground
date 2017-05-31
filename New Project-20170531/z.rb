ce2529966a6e218451f8b8886b5133777d9407b5
bogdangutsol <bgutsol@gmail.com>
2017-05-31 17:26:31 +0300

    Created by Coding Ground Client

---
+++ New Project-20170531/z.rb
@@ -0,0 +1,10 @@
+array=["Х","Л","Е","Б"]
+goes_on_a_visit=array
+puts 'Если в слове'
+puts array
+puts "сделать четыре ошибки, то получться" 
+goes_on_a_visit[0]=["П"]
+goes_on_a_visit[1]=["И"]
+goes_on_a_visit[2]=["В"]
+goes_on_a_visit[3]=["О"]
+puts array
